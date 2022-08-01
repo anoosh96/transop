2b759e4efbe017a54887a1e736ae41924231d8d0
Kirill Afan <kirka121@gmail.com>
2022-08-01 14:59:03 -0400

    style drawer list items

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -14,7 +15,8 @@ const MaterialComponents = [
   MatInputModule,
   MatToolbarModule,
   MatSidenavModule,
-  MatMenuModule
+  MatMenuModule,
+  MatListModule
 ]; 
 
 @NgModule({
